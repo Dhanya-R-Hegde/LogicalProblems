560d72d7141bb1dcf0e527c6024655556456f3e8
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-15 14:46:35 +0530

    [Dhanya R Hegde] : Added Fibanocci Problem

---
+++ Fibanocci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProblems
+{
+    internal class Fibanocci
+    {
+        public int num1 = 0, num2 = 1;
+        public int count = 0;
+
+        public void checkFibanocci(int n)
+        {
+            while (count < n)
+            {
+                Console.Write(num1 + " ");
+
+                int num3 = num2 + num1;
+                num1 = num2;
+                num2 = num3;
+
+                count++;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibanocci fibanocci = new Fibanocci();
+            fibanocci.checkFibanocci(5);
+        }
+    }
+}
