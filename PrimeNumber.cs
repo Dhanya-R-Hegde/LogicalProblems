3e4fbe581da1a96dc696f90421b9fa3c5a2a538e
Dhanya-R-Hegde <1rn19ec037.dhanyarhegde@gmail.com>
2023-09-15 15:10:40 +0530

    [Dhanya R Hegde] : Added PrimeNumber Problem

---
+++ PrimeNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicProblems
+{
+    internal class PrimeNumber
+    {
+        public void checkPrime(int num)
+        {
+            int count = 0;
+            if (num > 1)
+            {
+                for (int i = 2; i < num; i++)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 0)
+            {
+                Console.WriteLine("The given number is prime");
+            }
+            else
+            {
+                Console.WriteLine("The given number is not prime");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@
     {
         static void Main(string[] args)
         {
-            //Fibanocci fibanocci = new Fibanocci();
-            //fibanocci.checkFibanocci(5);
+            Fibanocci fibanocci = new Fibanocci();
+            fibanocci.checkFibanocci(5);
 
             PerfectNumber perfectNumber = new PerfectNumber();
             perfectNumber.checkPerfectNum(14);
+
+            PrimeNumber primeNumber = new PrimeNumber();
+            primeNumber.checkPrime(5);
         }
     }
 }
